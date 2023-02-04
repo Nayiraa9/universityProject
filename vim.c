60eecaba459f70316b30dcb76770c78f6ce6b383
Nayiraa9 <118961526+Nayiraa9@users.noreply.github.com>
2023-02-05 01:40:30 +0330

    Update vim.c

--- vim.c
+++ vim.c
@@ -6,4 +6,48 @@
 #include <stdbool.h>
 // for getch
 #include <conio.h>
+/**
+ * @brief Content struct
+ * 
+ * @param name
+ * @param content
+ * @param lines
+ * @param line_count
+ * 
+ */
+typedef struct
+{
+    char* name;
+    char* content;
+    char** lines;
+    int line_count;
+} Content;
+
+/**
+ * @brief Position struct
+ * 
+ * @param x
+ * @param y
+ * 
+ */
+typedef struct
+{
+    unsigned int x;
+    unsigned int y;
+} Position;
+
+/**
+ * @brief Mode enum
+ * 
+ * @param MODE_VISUAL
+ * @param MODE_INSERT
+ * @param MODE_COMMAND
+ * 
+ */
+typedef enum
+{
+    MODE_VISUAL,
+    MODE_INSERT,
+    MODE_COMMAND
+} Mode;
 
